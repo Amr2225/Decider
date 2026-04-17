7d81267bd3ca5a4bb2f851859f5c571d0eb5f924
Amr <amrmohamed.amaa@gmail.com>
2026-04-17 20:05:32 +0200

    Simple prisma config

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Prisma, User } from 'generated/prisma/client';
 
 // Auth -- Method 2
 @Controller()
@@ -9,5 +10,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("create-user")
+  createUser(): Promise<User> {
+    const data: Prisma.UserCreateInput = {
+      name: "John Doe",
+      email: "john.doe@example.com",
+    }
+
+
+    return this.appService.createUser(data);
+  }
+
+  @Get("get-users")
+  getUsers(): Promise<User[]> {
+    return this.appService.getUsers();
   }
 }
--- apps/backend/src/app.service.ts
+++ apps/backend/src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Prisma, User } from '../generated/prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) { }
+
+
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }
